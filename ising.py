ba81ce6f51ffd8da183a23695237843016988439
Josja Van Bever <josja.vanbever@gmail.com>
2019-05-11 17:50:51 +0200

    working diagonalization!

--- ising.py
+++ ising.py
@@ -49,10 +49,10 @@ class H_TFIM():
         self.L = L
         self.J = J
         self.g = g
-        size = 1 << L
-        self.mat = np.zeros((size, size))
+        self.dim = 1 << L
+        self.mat = np.zeros((self.dim, self.dim))
         # nearest neighbour term
-        for basis in range(size):
+        for basis in range(self.dim):
             M = 0
             for site in range(L - 1):
                 # true if site and site + 1 are anti-parallel
@@ -61,7 +61,8 @@ class H_TFIM():
                 else:
                     M -= 1
             # periodic boundary conditions
-            if (basis ^ basis >> L) & 1:
+            # skip this part for open boundary conditions
+            if (basis ^ basis >> (L - 1)) & 1:
                 M += 1
             else:
                 M -= 1
@@ -69,8 +70,8 @@ class H_TFIM():
             self.mat[basis, basis] = J * M
 
         # transvers field term
-        for basis in range(size):
-            for site in range(L - 1):
+        for basis in range(self.dim):
+            for site in range(L):
                 # spinflip at 'site' and factor -Jg
                 self.mat[basis ^ (1 << site), basis] = -J * g
 
@@ -79,60 +80,51 @@ class H_TFIM():
         return "-%.1f * [∑_{<ij>}^%d σ^z_i σ^z_j + %.1f * ∑_{i}^%d  σ^x_i]\n%r" \
                 % (self.J, self.L, self.g, self.L, self.mat)
 
-def main():    
+def main():
+    # set printing format for numpy
+    np.set_printoptions(precision=1, suppress=True, threshold=100)
+
     # read the parameters from input
     # L: the amount of sites
     # J, g: parameters of H_{TFIM}
     L, J, g = get_options()
 
-    print(L, J, g)
+    # create the hamiltonian
     hamiltonian = H_TFIM(L, J, g)
+
     print(hamiltonian)
 
-    size = 1 << L
-    ham = np.zeros((size, size))
-    # nearest neighbour term
-    for basis in range(size):
-        M = 0
-        for site in range(L - 1):
-            # true if site and site + 1 are anti-parallel
-            if (basis >> site ^ basis >> (site + 1)) & 1:
-                M += 1
-            else:
-                M -= 1
-        # periodic boundary conditions
-        if (basis ^ basis >> L) & 1:
-            M += 1
-        else:
-            M -= 1
-        # factor J * [# anti-parallel - # parallel]
-        ham[basis, basis] = J * M
+    # diagonalize the hamiltonian
+    eigenvalues, eigenvectors = eigsh(hamiltonian.mat, k=min(30,hamiltonian.dim-1), which='SA')
 
-    # transvers field term
-    for basis in range(size):
-        for site in range(L - 1):
-            # spinflip at 'site' and factor -Jg
-            ham[basis ^ (1 << site), basis] = -J * g
-
-    print(ham)
-
-    identity = np.eye(8)
-    #print(identity)
-    #test = np.array([0, 5, 6, 8, 7, 9, 4, 5])
-    #print(identity * test[0])
-    #print(identity * test)
-    #print(np.dot(identity,test))
-    #print(np.einsum('ij,j->i', identity, test))
-    eigenvalues, eigenvectors = eigsh(hamiltonian.mat, k=6)
+    # print the eigenvalues
     print(eigenvalues)
-    #print(eigenvectors.shape)
+    # print the eigenvectors
+    for i in range(len(eigenvalues)):
+        print('{0:.2f}: '.format(eigenvalues[i]), eigenvectors[:, i])
+    '''
+    for i in range(min(6,len(eigenvectors))):
+        vector = eigenvectors[i]
+        for b in range(len(vector)):
+            basis = vector[b]
+            if abs(basis) > 0.001:
+                print("Eigenvector {0:d}: {1:10b}".format(i, b))
+    '''
 
+# don't run main if included 
 if __name__ == "__main__":
     main()
 
 # customized build
 # { "shell_cmd": "python3 ising.py -L8 -J1 -g0.5"}
 
+ #identity = np.eye(8)
+    #print(identity)
+    #test = np.array([0, 5, 6, 8, 7, 9, 4, 5])
+    #print(identity * test[0])
+    #print(identity * test)
+    #print(np.dot(identity,test))
+    #print(np.einsum('ij,j->i', identity, test))
 
 # print("check: " + str(3 >> 1))
 #     print("check: " + str(3 >> 0))
@@ -140,3 +132,5 @@ if __name__ == "__main__":
 #     print("check: " + str(2 & 1))
 #     print("check: " + str(0 >> 0 ^ 0 >> 1))
 #     print("check: " + str((0 >> 0 ^ 0 >> 1) & 1))
+
+# print("basis: {0:b}".format(basis))
