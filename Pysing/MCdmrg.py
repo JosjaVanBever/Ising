51bba9e31ee86b17ab23e00dbbd44be87d88d6fb
Josja Van Bever <josja.vanbever@gmail.com>
2019-05-13 10:38:12 +0200

    index shift seems succesfull

--- Pysing/MCdmrg.py
+++ Pysing/MCdmrg.py
@@ -74,26 +74,32 @@ def main():
 
     print(N, J, g, D)
 
+    # REMARK: site indexing starts at 0 and ends at N-1!
+    forwards  = range(N)           # 0 ... N-1
+    backwards = range(N-1,-1,-1)   # N-1 ... 0
+
     # for each of N sites, keep 2 matrices (for up/down)
     # Remark that these matrices are real symmetric and have common
     # eigenvalues (due to spin symmetry), but does not lead to
     # normalized probabilities p_i: ∑_i p_i != 1.
     # Expectation values should therefore always be normalized.
-    site_matrices = [None] + [site.random(D) for i in range(N)]
+    site_matrices = [site.random(D) for i in forwards]
     # due to the dummy, site_matrices[1] corresponds to the 1st site
 
-    # site_matrices print as check
-    for i in range(1,len(site_matrices)):
-        print('Site {0:d}:\n'.format(i), site_matrices[i])
+    print(site_matrices)
+    # # site_matrices print as check
+    # for i in range(1,len(site_matrices)):
+    #     print('Site {0:d}:\n'.format(i), site_matrices[i])
 
     # the current state of the system is described by an array
     # of bits: 0 = up (= σ^z[0,0]), 1 = down (= σ^z[1,1])
     # initialize at a random state
-    state = [None] + [random.randint(2) for i in range(N)]
+    state = [random.randint(2) for i in forwards]
     # remark that state[1] corresponds to the first site
 
     # state print as check
-    print([state[i] for i in range(1,len(state))])
+    print(state)
+    # print([state[i] for i in range(1,len(state))])
 
     # help function to get the current matrix for site i
     def current_site_mat(i):
@@ -101,23 +107,23 @@ def main():
 
     # RL_matrices[m] = L(m) or R(m) depending on the conditions
     # initialize L(m) = A(s_m), L/R(0) = L/R(N+1) = 1
-    RL_matrices = [np.eye(D)] + [current_site_mat(i)
-        for i in range(1,len(site_matrices))] + [np.eye(D)]
-    # remark that RL_matrices[1] corresponds to site m=1
+    RL_matrices = [current_site_mat(i) for i in forwards] \
+                + [np.eye(D)]
+    # remark that RL_matrices[0] corresponds to site m=0
 
     # L(m) = A(s_m) * L(m+1), m from N-1 to 1
-    for i in range(N,0,-1):  # incl. N, excl. 0
+    for i in backwards:
         RL_matrices[i] = matmul(RL_matrices[i], RL_matrices[i+1])
 
     # wave function coefficients for each state S
-    W = np.zeros(F)    # W(S) = Tr(A(s_1)...A(s_N))
+    W = np.zeros(F)    # W(S) = Tr(A(s_0)...A(s_(N-1)))
     # estimators for the energy contributions to H_{TFIM}:
     E_z = np.zeros(F)  # E_z(S) = J * ∑_{sites} *
                        # [# anti-parallel(S) - # parallel(S)]
     E_x = np.zeros(F)  # E_x(S) = Jg * ∑_{m} W(S'_m)/W(S)
     # estimator for the derivatives of the energy
-    X_S = np.array(['None'] + [site.zeros(D) for i in range(N)])
-    Wderiv = [np.zeros((D,D)) for i in range(F)]   # WRONG DIMENSIONS!!!
+    X_S = np.array([site.zeros(D) for i in forwards])
+#    Wderiv = [np.zeros((D,D)) for i in range(F)]   # WRONG DIMENSIONS!!!
     # we should collect matrices per sweep S, per site m, per spin up/down at this site!!!
         # W(S) * Δ(S) = ∂W(S)/∂A(S)
         # <=> W(S) * [Δ(S)]^s_{ij} = ∂W(S)/∂[A(S)]^s_{ij}
@@ -125,12 +131,12 @@ def main():
     # do one simulation bin; i.e. collect enough information
     # to calculate the ensemble averages needed to update W(S)
     for S in range(F):
-        # remark that W(s) = Tr(A(s_1)...A(s_N)) = Tr(L(1))
-        W[S] = np.trace(RL_matrices[1]) # := W(S)
+        # remark that W(s) = Tr(A(s_0)...A(s_(N-1))) = Tr(L(0))
+        W[S] = np.trace(RL_matrices[0]) # := W(S)
         # Wp[m] (p='prime') := W(S'_m) = W after flipping spin s_m
-        Wp = [None] + [0 for i in range(N)]
-        # traverse from s_1 to s_N and do metropolis based flips:
-        for m in range(1,len(state)):
+        Wp = [0 for i in range(N)]
+        # traverse from s_0 to s_(N-1) and do metropolis based flips:
+        for m in forwards:
             # B(m) = L(m+1)*R(m-1)
             Bm = matmul(RL_matrices[m+1], RL_matrices[m-1])
             # ∂W(S)/∂[A(S)]^s_{ij} = ∑_{m} [(B(m) + B(m)^t)/2]_{ij}
@@ -140,27 +146,22 @@ def main():
             # otherwise, add to Wderiv[sweep S, site m, matrix down]
 #            Wderiv[S][m][state[m]] += (Bm + Bm.T) / 2
             # get A(-s_m)
-            Aflip = site_matrices[i].get(1-state[i])
+            Aflip = site_matrices[m].get(1-state[m])
             # W(S'_m) = Tr(A(-s_m)*B(m))
             Wp[m] = np.trace(matmul(Aflip, Bm))
             # change of flipping a spin
             Pflip = min((Wp[m]*Wp[m])/(W[S]*W[S]) ,1)
             # God roles a dice,
             if random.random() < Pflip:
-                state[i] = 1 - state[i]  # and flips a spin
+                state[m] = 1 - state[m]  # and flips a spin
             # R(m) = R(m-1) * A(s_m)
             RL_matrices[m] = matmul(RL_matrices[m-1], current_site_mat(m))
 
         # get the estimator E_z(S) for the diagonal contribution to H_TFIM
         M = 0
-        for i in range(1,len(state)-1):
+        for i in forwards:
             # true if anti-parallel
-            if state[i] ^ state[i+1]:
-                M += 1
-            else:
-                M -= 1
-            # periodic boundary conditions
-            if state[1] ^ state[N]:
+            if state[i] ^ state[i-1]:  # incl. periodic boundary conditions
                 M += 1
             else:
                 M -= 1
@@ -168,10 +169,11 @@ def main():
         E_z[S] = J * M
 
         # get the estimator E_x(S) for the non diagonal contribution to H_TFIM
-        E_x[S] = J * g *sum([Wp[i] for i in range(1,len(Wp))]) / W[S]
+        E_x[S] = J * g * sum(Wp) / W[S]
+#        E_x[S] = J * g *sum([Wp[i] for i in range(1,len(Wp))]) / W[S]
 
         # transverse from s_N to s_1 and evaluate the energy and its derivative:
-        for m in range(N,0,-1):
+        for m in backwards:
             # L(m) = A(s_m) * L(m+1)
             RL_matrices[m] = matmul(current_site_mat(m), RL_matrices[m+1])
 
