d916bcc1fb3ccf6a523b4a38cf18eec12723651e
Josja Van Bever <josja.vanbever@gmail.com>
2019-05-13 11:09:47 +0200

    cleaning up after index shift

--- Pysing/MCdmrg.py
+++ Pysing/MCdmrg.py
@@ -74,7 +74,7 @@ def main():
 
     print(N, J, g, D)
 
-    # REMARK: site indexing starts at 0 and ends at N-1!
+    # REMARK: site indexing starts at 0 and ends at N-1
     forwards  = range(N)           # 0 ... N-1
     backwards = range(N-1,-1,-1)   # N-1 ... 0
 
@@ -84,34 +84,22 @@ def main():
     # normalized probabilities p_i: ∑_i p_i != 1.
     # Expectation values should therefore always be normalized.
     site_matrices = [site.random(D) for i in forwards]
-    # due to the dummy, site_matrices[1] corresponds to the 1st site
-
-    print(site_matrices)
-    # # site_matrices print as check
-    # for i in range(1,len(site_matrices)):
-    #     print('Site {0:d}:\n'.format(i), site_matrices[i])
 
     # the current state of the system is described by an array
     # of bits: 0 = up (= σ^z[0,0]), 1 = down (= σ^z[1,1])
     # initialize at a random state
     state = [random.randint(2) for i in forwards]
-    # remark that state[1] corresponds to the first site
-
-    # state print as check
-    print(state)
-    # print([state[i] for i in range(1,len(state))])
 
     # help function to get the current matrix for site i
     def current_site_mat(i):
         return site_matrices[i].get(state[i])
 
     # RL_matrices[m] = L(m) or R(m) depending on the conditions
-    # initialize L(m) = A(s_m), L/R(0) = L/R(N+1) = 1
+    # Initialize L(m) = A(s_m), L/R(-1) = L/R(N) = 1
     RL_matrices = [current_site_mat(i) for i in forwards] \
-                + [np.eye(D)]
-    # remark that RL_matrices[0] corresponds to site m=0
+                + [np.eye(D)]  # list[-1] == list[len(list)]
 
-    # L(m) = A(s_m) * L(m+1), m from N-1 to 1
+    # L(m) = A(s_m) * L(m+1)
     for i in backwards:
         RL_matrices[i] = matmul(RL_matrices[i], RL_matrices[i+1])
 
@@ -119,7 +107,7 @@ def main():
     W = np.zeros(F)    # W(S) = Tr(A(s_0)...A(s_(N-1)))
     # estimators for the energy contributions to H_{TFIM}:
     E_z = np.zeros(F)  # E_z(S) = J * ∑_{sites} *
-                       # [# anti-parallel(S) - # parallel(S)]
+                       #    [# anti-parallel(S) - # parallel(S)]
     E_x = np.zeros(F)  # E_x(S) = Jg * ∑_{m} W(S'_m)/W(S)
     # estimator for the derivatives of the energy
     X_S = np.array([site.zeros(D) for i in forwards])
@@ -130,21 +118,24 @@ def main():
 
     # do one simulation bin; i.e. collect enough information
     # to calculate the ensemble averages needed to update W(S)
+    # each step starts and ends with RL_matrices[m] = L(m)
     for S in range(F):
         # remark that W(s) = Tr(A(s_0)...A(s_(N-1))) = Tr(L(0))
         W[S] = np.trace(RL_matrices[0]) # := W(S)
         # Wp[m] (p='prime') := W(S'_m) = W after flipping spin s_m
-        Wp = [0 for i in range(N)]
+        Wp = [0 for i in forwards]
         # traverse from s_0 to s_(N-1) and do metropolis based flips:
         for m in forwards:
             # B(m) = L(m+1)*R(m-1)
             Bm = matmul(RL_matrices[m+1], RL_matrices[m-1])
+
             # ∂W(S)/∂[A(S)]^s_{ij} = ∑_{m} [(B(m) + B(m)^t)/2]_{ij}
             # ALARM: DISTINGUISHING UP AND DOWN????????????????????????
             # ASK A(s_m): ar you up or down?
             # when up, then add (Bm + Bm.T) / 2 to (delta_s,s_m in eq. (9)) to Wderiv[sweep S, site m, matrix up]
             # otherwise, add to Wderiv[sweep S, site m, matrix down]
 #            Wderiv[S][m][state[m]] += (Bm + Bm.T) / 2
+
             # get A(-s_m)
             Aflip = site_matrices[m].get(1-state[m])
             # W(S'_m) = Tr(A(-s_m)*B(m))
@@ -170,9 +161,8 @@ def main():
 
         # get the estimator E_x(S) for the non diagonal contribution to H_TFIM
         E_x[S] = J * g * sum(Wp) / W[S]
-#        E_x[S] = J * g *sum([Wp[i] for i in range(1,len(Wp))]) / W[S]
 
-        # transverse from s_N to s_1 and evaluate the energy and its derivative:
+        # transverse from s_(N-1) to s_0 and evaluate the energy and its derivative:
         for m in backwards:
             # L(m) = A(s_m) * L(m+1)
             RL_matrices[m] = matmul(current_site_mat(m), RL_matrices[m+1])
@@ -180,6 +170,7 @@ def main():
     # calculate the ensemble avarages
     Z = sum(W * W)                       # Z = ∑_{S} W²(S)    
     E = (1/Z) * sum(W*W * (E_z + E_x))   # E = <E_z(S) + E_x(S)>_{S}
+
     # <[Δ(S)]^s_{ij}> = 1/Z * ∑_{S} [W(S) * ∂W(S)/∂[A(S)]^s_{ij}]
 #    Delta = (1/Z) * sum(W * Wderiv)
     # ∂E/∂[A]^s_{ij} = 2/Z * ∑_{S} [W(S) * E(S) * ∂W(S)/∂[A(S)]^s_{ij}]
@@ -195,5 +186,3 @@ def main():
 # don't run main if included 
 if __name__ == "__main__":
     main()
-
-# RL_matrices = [np.eye(D)] + [mat_i.rand() for mat_i in site_matrices] + [np.eye(D)]
